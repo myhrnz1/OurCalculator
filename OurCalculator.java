c4b0ef003947732b23d33a5720a5a946e6457513
myhrnz1 <myhrnz@outlook.com>
2018-11-20 14:53:18 +0100

    add imports and class variables

--- OurCalculator.java
+++ OurCalculator.java
@@ -1,55 +1,13 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class OurCalculator {
+  private static double firstNumber = 0;
+  private static double secondNumber = 0;
+  private static double answer = 0;
+  private static String operator = "-1";
 
-  private static void add(){
-   answer = firstNumber + secondNumber;
-  }
-  
-  private static void sub(){
-    answer = firstNumber - secondNumber;
-  }
-
-  private static void mult(){
-    answer = firstNumber * secondNumber;
-  }
-
-  private static void div(){
-    answer = firstNumber / secondNumber;
-  }
-
-  private static double enterDouble(){
-    double input = 0;
-    boolean error = true;
-    System.out.print("Enter number: ");
-    while(error) {
-      try {
-        input = scan.nextDouble();
-        error = false;
-      }
-      catch (InputMismatchException e) {
-        System.out.println("Input is not a double");
-        scan.next(); // clear scanner
-      }
-    }
-  return input;
-  }
-
-  private static void enterOperator(){
-    while(!(operator.equals("+") || operator.equals("-") || 
-             operator.equals("*") || operator.equals("/"))) {
-      System.out.print("Enter operator: ");
-      operator = scan.next();
-    }
-  }
-
-  private static void printResult(){
-    System.out.println("The answer is: " + answer);
-  }
-
-  private static void printInstructions(){
-    System.out.println("Step 1: Enter a number.");
-    System.out.println("Step 2: Enter an operator.");
-    System.out.println("Step 3: Enter a number.");
-  }
+  static Scanner scan = new Scanner(System.in);
 
   public static void main(String[] args) {
   }
