724810a7fe01fd9d3ed9cfd4cc3e0c46d57ee587
JonasPersson78 <jonasnpersson@gmail.com>
2018-11-20 14:17:20 +0100

    Add Main to Project

--- OurCalculator.java
+++ OurCalculator.java
@@ -1,4 +1,27 @@
-public class OurCalculator {
-  public static void main(String[] args) {
+public static void main(String[] args){
+  printInstructions();
+  String quit = "n";
+  while(!quit.equals("y")){
+    firstNumber = enterDouble();
+    enterOperator();
+    secondNumber = enterDouble();
+    if (!(operator.equals("/") && secondNumber == 0)){
+      if ( operator.equals("+") ) {
+        add();
+      }else if (operator.equals("-") ){
+        sub();
+      }else if (operator.equals("*") ){
+        mult();
+      }else if (operator.equals("/") ){
+        div();
+    }
+    printResult();
+  } else {
+    System.out.println("You are dividing by Zero! Try agin!: ");
   }
-}
+    System.out.println("Qiut? 'Y' for Yes");
+    quit = scan.next();
+    operator = "-1";
+
+  }
+ }
